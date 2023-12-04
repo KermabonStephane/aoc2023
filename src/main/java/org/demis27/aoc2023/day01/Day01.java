6a3311bc2f9555329e0075a7f13747ed3a841112
Stéphane Kermabon <stephane.kermabon.partner@decathlon.com>
2023-12-04 14:43:06 +0100

    Add day 01

--- src/main/java/org/demis27/aoc2023/day01/Day01.java
+++ src/main/java/org/demis27/aoc2023/day01/Day01.java
@@ -10,7 +10,28 @@ import java.util.concurrent.atomic.AtomicLong;
 @Slf4j
 public class Day01 {
 
-    public long getResult(final String filename) throws IOException {
+    public long getResultPartOne(final String filename) throws IOException {
+        AtomicLong result = new AtomicLong();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(filename)))) {
+
+            String line = reader.readLine();
+            result.set(0L);
+
+            while (line != null) {
+                String numbers = line.replaceAll("[a-zA-A]", "");
+                if (numbers.length() > 1) {
+                    result.addAndGet(Long.parseLong(numbers.substring(0, 1)) * 10L + Long.parseLong(numbers.substring(numbers.length() - 1)));
+                } else if (numbers.length() == 1) {
+                    result.addAndGet(Long.parseLong(numbers.substring(0, 1)) * 11L);
+                }
+                line = reader.readLine();
+            }
+        }
+        log.info("result {}", result.get());
+        return result.get();
+    }
+
+    public long getResultPartTwo(final String filename) throws IOException {
         AtomicLong result = new AtomicLong();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(filename)))) {
 
--- src/test/java/org/demis27/aoc2023/day01/Day01Test.java
+++ src/test/java/org/demis27/aoc2023/day01/Day01Test.java
@@ -8,9 +8,29 @@ import java.io.IOException;
 class Day01Test {
 
     @Test
-    void day1() throws IOException {
+    void day1_part1_sample() throws IOException {
         Day01 day01 = new Day01();
-        long result = day01.getResult("day1.txt");
+        long result = day01.getResultPartOne("day1-part1-sample.txt");
+        Assertions.assertEquals(142, result);
+    }
+
+    @Test
+    void day1_part2_sample() throws IOException {
+        Day01 day01 = new Day01();
+        long result = day01.getResultPartTwo("day1-part2-sample.txt");
+        Assertions.assertEquals(281, result);
+    }
+    @Test
+    void day1_part1() throws IOException {
+        Day01 day01 = new Day01();
+        long result = day01.getResultPartOne("day1.txt");
+        Assertions.assertEquals(53921, result);
+    }
+
+    @Test
+    void day1_part2() throws IOException {
+        Day01 day01 = new Day01();
+        long result = day01.getResultPartTwo("day1.txt");
         Assertions.assertEquals(54676, result);
     }
 }
