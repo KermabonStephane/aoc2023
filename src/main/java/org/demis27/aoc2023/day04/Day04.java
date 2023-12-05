b2a2f76e56b7d07bf9e6e95531cd96280eb3320d
Kermabon Stéphane <demis27@demis27.net>
2023-12-05 17:46:10 +0100

    Clean import

--- src/main/java/org/demis27/aoc2023/day04/Day04.java
+++ src/main/java/org/demis27/aoc2023/day04/Day04.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.javatuples.Pair;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
