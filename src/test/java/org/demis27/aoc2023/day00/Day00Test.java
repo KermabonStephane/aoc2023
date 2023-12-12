3a7824768866e4cb03f40045ff8641ca87e0e87a
Stéphane Kermabon <stephane.kermabon.partner@decathlon.com>
2023-12-12 22:29:39 +0100

    Day 13 Prepare

--- src/test/java/org/demis27/aoc2023/day00/Day00Test.java
+++ src/test/java/org/demis27/aoc2023/day00/Day00Test.java
@@ -15,18 +15,18 @@ public class Day00Test {
     }
 
     @Test
-    void day00_part2_sample() throws IOException {
-        Day00 day00 = new Day00();
-        long result = day00.processPartTwo("day00-part2-sample.txt");
-        Assertions.assertEquals(0, result);
-    }
-    @Test
     void day00_part1() throws IOException {
         Day00 day00 = new Day00();
         long result = day00.processPartOne("day00.txt");
         Assertions.assertEquals(0, result);
     }
 
+    @Test
+    void day00_part2_sample() throws IOException {
+        Day00 day00 = new Day00();
+        long result = day00.processPartTwo("day00-part2-sample.txt");
+        Assertions.assertEquals(0, result);
+    }
     @Test
     void day00_part2() throws IOException {
         Day00 day00 = new Day00();
