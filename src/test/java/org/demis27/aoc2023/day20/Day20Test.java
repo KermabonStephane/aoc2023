98808c96c71323d2c33237340ab7b5004d109ae3
Stéphane Kermabon <stephane.kermabon.partner@decathlon.com>
2023-12-19 23:19:29 +0100

    Prepare Day 20

---
+++ src/test/java/org/demis27/aoc2023/day20/Day20Test.java
@@ -0,0 +1,38 @@
+package org.demis27.aoc2023.day20;
+
+import org.demis27.aoc2023.day00.Day00;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Day20Test {
+
+    @Test
+    void day00_part1_sample() throws IOException {
+        Day00 day00 = new Day00();
+        long result = day00.processPartOne("day00-part1-sample.txt");
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void day00_part1() throws IOException {
+        Day00 day00 = new Day00();
+        long result = day00.processPartOne("day00.txt");
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void day00_part2_sample() throws IOException {
+        Day00 day00 = new Day00();
+        long result = day00.processPartTwo("day00-part2-sample.txt");
+        Assertions.assertEquals(0, result);
+    }
+    @Test
+    void day00_part2() throws IOException {
+        Day00 day00 = new Day00();
+        long result = day00.processPartTwo("day00.txt");
+        Assertions.assertEquals(0, result);
+    }
+
+}
